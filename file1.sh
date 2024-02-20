e2fbadedf9e203fbe5546032990a0f9cec0f4ff2
Hazem Al Indari <hazemalindari@Hazems-MacBook-Air.local>
2024-02-20 20:04:13 +0100

    6

--- file1.sh
+++ file1.sh
@@ -7,5 +7,12 @@ conda install -y -n base conda-libmamba-solver
 conda config --set solver libmamba
 conda install -y -n base conda-lock==1.4.0
 conda activate base
-
-#cd chipyard/
+cd chipyard/
+git switch --detach 1.11.0
+./build-setup.sh riscv-tools --skip-firesim --skip-marshal
+cd ..
+apt-get install verilator
+cd chipyard/
+source ./env.sh
+cd sims/verilator
+make
