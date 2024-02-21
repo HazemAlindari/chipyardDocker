3b80f7322667d69d08c19f19d23817c2ce25d16d
Hazem Al Indari <hazem_jaki@hotmail.com>
2024-02-21 16:17:14 +0100

    fixed entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 # used with the dockerfile to set up enviroment variables by running env.sh
 # adapted from https://stackoverflow.com/questions/55921914/how-to-source-a-script-with-environment-variables-in-a-docker-build-process
 
-. chipyard/env.sh
-
+source "${HOME}/conda/etc/profile.d/conda.sh"
+conda activate
+source chipyard/env.sh
 exec "$@"
